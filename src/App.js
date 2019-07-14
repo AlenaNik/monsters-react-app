b93de044224e9a06e7993aea474496d63bc39ed5
AlenaNik <alena.nik.digital@gmail.com>
2019-07-14 20:33:47 +0200

    fetch with web api, users jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hi!</h1>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+        this.state = {
+            users: []
+        };
+    }
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => (
+                res.json())
+                .then(users => this.setState({users: users}))
+            );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {
+                    this.state.users.map(user => <h1 key={user.id}>{ user.name }</h1>)
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
