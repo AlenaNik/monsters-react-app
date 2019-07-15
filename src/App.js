3feccbacb2a2b149333f92d19e49aef18b85cdfa
AlenaNik <alena.nik.digital@gmail.com>
2019-07-15 18:41:23 +0200

    monsters card added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component'
 
 class App extends Component {
     constructor(){
         super();
         this.state = {
-            users: []
+            monsters: []
         };
     }
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(res => (
                 res.json())
-                .then(users => this.setState({users: users}))
+                .then(users => this.setState({monsters: users}))
             );
     }
 
     render() {
         return (
             <div className="App">
-                {
-                    this.state.users.map(user => <h1 key={user.id}>{ user.name }</h1>)
-                }
+                <CardList monsters={this.state.monsters}>
+            </CardList>
             </div>
         );
     }
